aa3ca41432d158556e3fc9323d08c2507c1e8bd0
Quinn Schwartz and Ryan Peterson <quinn+ryan@>
2017-01-10 14:20:26 -0800

    add update function.

--- app/routes/story.js
+++ app/routes/story.js
@@ -8,6 +8,15 @@ export default Ember.Route.extend({
     destroyStory(story) {
       story.destroyRecord();
       this.transitionTo('index');
-    }  
+    },
+    update(story, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!== undefined) {
+          story.set(key,params[key]);
+        }
+      });
+      story.save();
+      this.transitionTo('index');
+    }
   }
 });
